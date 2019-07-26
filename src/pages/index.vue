a9e42a4c175e234f94a48870de5f6bae4e2695ce
UncleFan <644742791@qq.com>
2019-07-26 18:01:19 +0800

    第一次提交，模板搭建，插件安装引入。

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    data(){
+        return {
+            msg: "首页"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
